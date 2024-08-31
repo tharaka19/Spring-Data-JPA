93179855faac7ca7ee62c134e692918db9a48480
Shehan Tharaka <shehantharaka000@gmail.com>
2024-08-31 21:14:32 +0530

    Initial commit

---
+++ src/test/java/com/demo/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
